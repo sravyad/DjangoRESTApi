75f45d6b217bdf19a550d32558860416edc3d504
Sravya Dogiparthi (sravd) <sravd@cisco.com>
2022-08-17 12:29:58 +0530

    Created super user n accessed from Admin UI

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -23,7 +23,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         """create a new admin with  the given details"""
         user=self.create_user(email,name,password)
         user.is_staff=True
